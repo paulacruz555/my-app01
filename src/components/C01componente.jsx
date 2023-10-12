92bf0f623dfd108e457325e9b3f0aa465c9ae6f7
sandra <sandrapcmer@gmail.com>
2023-10-11 20:49:29 -0500

    v03 componente 1

--- src/components/C01componente.jsx
+++ src/components/C01componente.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 export const C01componente = () => {
   return (
     <div>
-      <h1>Primer Componente</h1>
+      
       Primer componente definido...
+      <br></br>
+      Hola Mundo  desde React
 
     </div>
     
